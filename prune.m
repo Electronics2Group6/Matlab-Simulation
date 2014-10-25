0933f7f5c7f513feb660bf3f5364a9ef667a0561
Andrew Gaus <andrewgaus815@gmail.com>
2014-10-25 17:21:48 -0400

    fixed pruning function

--- prune.m
+++ prune.m
@@ -4,10 +4,9 @@ function y = prune(y, k)
 B = zeros(8, 8);
 Y = fliplr(y);
 % for j=k:-1:-8
-% j=k:-1:-8
 %   y2 = spdiags(B,j,Y);
 %   y = full(y2)
 % end
 
-Y = triu(Y,k);
+% Y = triu(Y,k);
 y = fliplr(Y);
