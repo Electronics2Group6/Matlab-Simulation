4cf98fcfd1fe1e7d6cb6663af87f6d9dc273bab9
Strang84 <strang84@students.rowan.edu>
2014-10-25 15:49:53 -0400

    Update prune.m
    Added a description of the function.

--- prune.m
+++ prune.m
@@ -1,9 +1,9 @@
 function y = prune(y, k)
+%Sets how much of the image pixels is saved from the original and used to 
+%reconstruct the image.
 B = zeros(8, 8);
 Y = fliplr(y);
 for j=k:-1:-8
-  y2 = spdiags(b,j,Y);
-end
-  
+y2 = spdiags(b,j,y);
 y = full(y2);
 y = fliplr(y);
